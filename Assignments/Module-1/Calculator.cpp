21add3a64c3173b024b6e045d5fe09e96ef44c9a
11Bhagirath <gajera.bhagirath@gmail.com>
2025-02-22 11:55:22 +0530

    Module-2

---
+++ Assignments/Module-1/Calculator.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a=88, b=25;
+	printf("Value of A is:%d",a);
+	printf("\nValue of B is:%d",b);
+	printf("\nAdd:%d",a+b);
+	printf("\nSub:%d",a-b);
+	printf("\nMul:%d",a*b);
+	printf("\nDiv:%d",a/b);
+}
